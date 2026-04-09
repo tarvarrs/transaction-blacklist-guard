2305ded8615121ad6d9c8f6c9873976138d839e0
tarvarrs <125196841+tarvarrs@users.noreply.github.com>
2026-04-09 21:38:43 +0300

    feat(db): change blacklist.id type to varchar

--- init-scripts/00-init-database.sql
+++ init-scripts/00-init-database.sql
@@ -1,12 +1,12 @@
 create table if not exists blacklist
 (
-    id integer
+    id varchar
 );
 
 create index idx_blacklist_id on blacklist (id);
 
-insert into blacklist(id) values(1);
-insert into blacklist(id) values(2);
-insert into blacklist(id) values(3);
-insert into blacklist(id) values(4);
-insert into blacklist(id) values(5);
+insert into blacklist(id) values('1');
+insert into blacklist(id) values('2');
+insert into blacklist(id) values('3');
+insert into blacklist(id) values('4');
+insert into blacklist(id) values('5');
